31f98e733f0f6759b35feee11edfa1209d952bde
Sascha <sascha.radatz@gmail.com>
2018-04-13 17:01:43 +0200

    initial commit x)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WhitepaperComponent } from './whitepaper/whitepaper.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WhitepaperComponent,
+    LeftMenuComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ {
+      provide: APP_BASE_HREF,
+      useValue: (window.location.pathname || '') // FIX FOR IPFS !!
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/whitepaper/whitepaper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-whitepaper',
+  templateUrl: './whitepaper.component.html',
+  styleUrls: ['./whitepaper.component.css']
+})
+export class WhitepaperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
